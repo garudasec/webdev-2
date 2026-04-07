0e24921a46c8dca0d5e2dacd90a1a66140ba48b8
Adarsh <adarshrathore.sec@gmail.com>
2026-04-07 15:05:41 +0530

    update

--- my-react-app/src/component/Sample.jsx
+++ my-react-app/src/component/Sample.jsx
@@ -1,43 +1,51 @@
-// import React from 'react'
-// import { add } from "./utility"
+// import React, { Component } from 'react'
 
-// function Sample(props) {
-//     console.log(add(2,4))
-//   return (
-//     <div>
-//       <h2>Trending Topics</h2>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod voluptatum iure incidunt, temporibus sequi iusto, pariatur maxime commodi ducimus, nihil autem. Magni modi praesentium, fuga culpa ad animi laboriosam!</p>
-//        <button
-//           className="counter"
-//           onClick={() => props.setCount((count) => count + 1)}
-//         >
-//           Count is {props.count}
-//         </button>
-//     </div>
-//   )
+//  class Sample extends Component {
+//     constructor(){
+//         super()
+//         this.state = {
+//             count: 0,
+//         }
+//     }
+//     increment = () => {
+//         this.setState({count: this.state.count + 1})
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is a sample component</h1>
+//         <h2>{this.state.count}</h2>
+//         <button onClick={this.increment}>Increment</button>
+//       </div>
+//     )
+//   }
 // }
 
 // export default Sample
 
-import React, {Component} from 'react'
 
-class Sample extends Component{
-    constructor(props){
-        super(props)
+import React from 'react'
+import { useState } from 'react'
+
+function Sample() {
+  // let count=0
+  const [count, setCount]=useState(0)
+  
+  const increment = () => {
+       setCount(count + 1)
     }
-    render(){
-        return (
-        <div>
-          <h2>Trending Topics</h2>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod voluptatum iure incidunt, temporibus sequi iusto, pariatur maxime commodi ducimus, nihil autem. Magni modi praesentium, fuga culpa ad animi laboriosam!</p>
-           <button
-            className="counter"
-            onClick={() => this.props.setCount((count) => count + 1)}
-            >
-            Count is {this.props.count}
-            </button>
-        </div>
-    )
+
+    const decrement = () => {
+       setCount(count - 1)
     }
+  return (
+    <div>
+      <h1>This is a sample component</h1>
+      <h2>{count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  )
 }
+
 export default Sample
